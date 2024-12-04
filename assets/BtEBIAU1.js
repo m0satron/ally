6392ee267626be8e52cff8a67ce332093bb03cde
m0satron <msebdani@gmail.com>
2024-12-04 17:10:26 +0100

    Updates

---
+++ assets/BtEBIAU1.js
@@ -0,0 +1,1 @@
+import{m as o,o as a,c as n,t as s,I as t,_ as r}from"./Czt5zaSC.js";const _=o({__name:"Item",props:{label:{},onDark:{type:Boolean}},setup(p){return(e,c)=>(a(),n("div",{class:t(["chip",{onDark:e.onDark}])},s(e.label),3))}}),m=r(_,[["__scopeId","data-v-22e28b4c"]]);export{m as _};
